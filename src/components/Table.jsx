c421e350cdb14630023ddf4d9a88aa9afdb42613
Rodrigo Castillo C <rodrigo.castillo.c@usach.cl>
2023-08-12 23:03:36 -0400

    Components: Se agrega funcionalidad que elimina el 'post' localmente en compontente 'Table'

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,9 +1,39 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { deletePost } from '../store/features/posts/postsSlice';
+import { showNotification, closeNotication } from '../store/features/notification/notificationSlice';
+import { setSuccess, setError, clearType } from '../store/features/type/typeSlice';
+import axios from 'axios'
 
 const Table = () => {
 
+  const apiUrl = process.env.REACT_APP_API_URL;
   let posts = useSelector(state => state.posts);
+  const dispatch = useDispatch();
+
+  const autoCloseNotification = () => {
+    setTimeout(() => {
+      dispatch(closeNotication());
+      dispatch(clearType());
+    }, 5000);
+  }
+
+  const removePost = async (e) => {
+    const idElement = e.target.parentElement.parentElement.id;
+    try {
+      const response = await axios.delete(apiUrl + '/posts/' + idElement);
+      const index = posts.findIndex(post => post.id === response.data.id);
+      dispatch(deletePost(index));
+      dispatch(setSuccess());
+      dispatch(showNotification('Se ha borrado exitósamente el post'));
+      autoCloseNotification();
+    } catch (error) {
+      dispatch(setError());
+      dispatch(showNotification(error.response.data.error));
+      autoCloseNotification();
+      console.error(error);
+    }
+  };
 
   return (
     <>
@@ -18,10 +48,10 @@ const Table = () => {
         <tbody>
           {
             posts.map(post =>
-              <tr key={post.id}>
+              <tr id={post.id} key={post.id}>
                 <td>{post.name}</td>
                 <td>{post.description}</td>
-                <td>Eliminar</td>
+                <td><a onClick={removePost}>Eliminar</a></td>
               </tr>
             )
           }
--- src/store/features/posts/postsSlice.js
+++ src/store/features/posts/postsSlice.js
@@ -3,17 +3,16 @@ import { createSlice } from '@reduxjs/toolkit';
 export const postsSlice = createSlice({
   name: 'posts',
   initialState: [
-    {id: 1, name: 'POST 1', description: 'Hola como estaás'},
-    {id: 2, name: 'POST 2', description: 'Hola como estaaaaaaaás'},
-    {id: 3, name: 'POST 3', description: 'Hola como estaaaaánnn'}
+    {id: 4, name: 'POST 1', description: 'Hola como estaás'},
+    {id: 5, name: 'POST 2', description: 'Hola como estaaaaaaaás'},
+    {id: 6, name: 'POST 3', description: 'Hola como estaaaaánnn'}
   ],
   reducers: {
     addPost: (state, action) => {
       state.push(action.payload);
     },
     deletePost: (state, action) => {
-      // Actualizar según corresponda
-      state.pop(action.payload);
+      state.splice(action.payload, 1);
     }
   }
 });
